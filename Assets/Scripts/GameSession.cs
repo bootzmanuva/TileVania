e5c0c9506346721f976d7d463af26a55da4e8d03
bootzmanuva <rugenr@gmail.com>
2021-08-25 14:37:49 -0700

    Main menu and Success screens
    Added "The End" scene and working buttons for that and main menu screen.

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] int playerLives = 3;
+
+    private void Awake()//keep session from being destroyed and destroy duplicates
+    {
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ProcessPlayerDeath()
+    {
+        if (playerLives > 1)
+        {
+            TakeLife();
+        }
+        else
+        {
+            ResetGameSession();
+        }
+    }
+
+    private void TakeLife()
+    {
+        playerLives--;
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    private void ResetGameSession()
+    {
+        SceneManager.LoadScene(1);
+        Destroy(gameObject);
+    }
+
+
+
+}
